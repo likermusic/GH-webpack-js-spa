6657c9e38019a3d344f93fca014357f454415fb6
Sergey Timofeev <drumliker@yandex.ru>
2023-11-26 19:30:16 +0300

    Сделали роутер

--- src/router.js
+++ src/router.js
@@ -1,39 +1,33 @@
-import render from './utils/render';
+import render from "./utils/render";
 
 function changePage() {
-  // console.log(history);
-  history.pushState({}, '', '/user/defunkt');
+  history.pushState({}, "", "/user/defunkt");
   routing();
 }
 
 export async function routing() {
   const routes = {
-    '/': 'home.js',
-    '/user/:login': 'user.js',
-    '/search': 'search.js',
-    404: '404.js',
+    "/": "home.js",
+    "/user/:login": "user.js",
+    "/search": "search.js",
+    404: "404.js",
   };
 
   const path = location.pathname; //   /   /profile  /search
 
   const page =
-    (path.startsWith('/user/') && routes['/user/:login']) ||
+    (path.startsWith("/user/") && routes["/user/:login"]) ||
     routes[path] ||
     routes[404]; // home.js
 
   const { markup, actions } = await import(`./pages/${page}`);
-  render(document.body, 'beforeend', markup, true);
+  render(document.body, "beforeend", markup, true);
 
   actions();
 
-
-
-  document.querySelector('.container').addEventListener('click', (e) => {
-    if (e.target.matches('.btn')) {
-      changePage();
-    }
-
-    //   e.preventDefault();
-    //   changePage();
-  });
+  // document.querySelector('.container').addEventListener('click', (e) => {
+  //   if (e.target.matches('.btn')) {
+  //     changePage();
+  //   }
+  // });
 }
